3ff6e98d7af8f266f3a255702b4490c1ecf47fc0
samatov914 <samatov914@gmail.com>
2022-12-30 21:07:32 +0600

    your commit

---
+++ dop_1.py
@@ -0,0 +1,36 @@
+#Args, kwargs
+# def employe(name1, name2, name3, name4):
+#     print("Здраствуйте",name1)
+#     print("Здраствуйте",name2)
+#     print("Здраствуйте",name3)
+#     print("Здраствуйте",name4)
+# employe("samat", "marat", "maks","bayka")
+
+# def arg_employe(*names):
+#     for name in names:
+#         print("Здраствуйте", name)
+# arg_employe("samat","marat", "maks","bayka",19)
+
+
+# def dictionary(**words):
+#     # print(words)
+#     for k, v in words.items():
+#         print(f"{k} - {v}")
+# dictionary(USA = "США", Apple = "Яблоко", Car = "Машина")
+
+# name = "Askhat"
+# geekcoin = 35
+# print(name + " " + str(geekcoin) + " geekcoin")
+# print(f"{name} {geekcoin} geekcoin")
+
+
+# a_lst = ["sasa","dsada", "19"]
+# a_str = " ".join(a_lst)
+# print(a_str)
+
+
+# name = "hello1"
+# print(name.isdigit())
+# print(name.isalnum())
+# print(name.islower())
+
